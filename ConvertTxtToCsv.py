54a2d6e4560463fb892be6ff71607f768d82d081
emceciliejensen <emmacbj@live.dk>
2025-08-13 11:59:03 +0200

    Update ConvertTxtToCsv.py

--- ConvertTxtToCsv.py
+++ ConvertTxtToCsv.py
@@ -4,7 +4,7 @@ columns = [
 	'PaperId:long', 'FieldOfStudyId:long', 'Score:float'
 ]
 
-chunk_size = 100000  # adjust if needed
+chunk_size = 100000 
 
 reader = pd.read_csv(
     "/home/emcj/data/MAG/PaperFieldsOfStudy.txt",
@@ -17,20 +17,6 @@ reader = pd.read_csv(
 )
 with open("PaperFieldsOfStudy.csv", "w") as f:
     for i, chunk in enumerate(reader):
-       	# Convert datetime columns
-       # for col in ['Date:DateTime?', 'OnlineDate:DateTime?', 'CreatedDate:DateTime']:
-        #	   chunk[col] = pd.to_datetime(chunk[col], errors='coerce')
-
-        # Convert numeric columns with possible missing values
-       # numeric_cols = [
-         #   'Rank:uint', 'Year:int?', 'JournalId:long?', 'ConferenceSeriesId:long?',
-          #  'ConferenceInstanceId:long?', 'ReferenceCount:long', 'CitationCount:long',
-           # 'EstimatedCitation:long', 'FamilyId:long?', 'FamilyRank:uint?'
-       # ]
-       # for col in numeric_cols:
-           # chunk[col] = pd.to_numeric(chunk[col], errors='coerce')
-
-        # Write chunk to CSV
         chunk.to_csv(f, index=False, header=(i == 0))
 
 
