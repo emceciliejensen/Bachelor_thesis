a45eb7cb5e00b3a64fe1a585e38f475bcbcf16e1
emceciliejensen <emmacbj@live.dk>
2025-08-13 12:27:04 +0200

    Update cleaned_authors.py

--- cleaned_authors.py
+++ cleaned_authors.py
@@ -3,19 +3,12 @@ import pandas as pd
 INPUT_PATH = "/home/emcj/data/MAG/AuthorsGenderized_merged.csv"
 OUTPUT_PATH = "/home/emcj/data/MAG/AuthorsGenderized_merged_clean.csv"
 
-# Load the data while skipping malformed lines
 df = pd.read_csv(INPUT_PATH, on_bad_lines='skip')
 print("read file")
-# Remove rows where gender is unknown (case insensitive)
 df = df[df['Gender'].str.lower() != 'unknown']
 print("dropped columns")
-# Drop duplicates based on Name (keep first)
 df_cleaned = df.drop_duplicates(subset="DisplayName", keep="first")
 print("dropped duplicates")
-# Save cleaned file
 df_cleaned.to_csv(OUTPUT_PATH, index=False)
 
-print(f"Cleaned data saved to: {OUTPUT_PATH}")
-print(f"Original rows (after skipping bad lines): {len(df)}")
-print(f"Rows after removing unknown genders and duplicates: {len(df_cleaned)}")
 
