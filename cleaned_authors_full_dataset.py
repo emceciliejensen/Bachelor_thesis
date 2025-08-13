60e6d7914e6553b50e8e1fed4bb1dd04595b1555
emceciliejensen <emmacbj@live.dk>
2025-08-13 12:27:35 +0200

    Update cleaned_authors_full_dataset.py

--- cleaned_authors_full_dataset.py
+++ cleaned_authors_full_dataset.py
@@ -4,14 +4,12 @@ INPUT_PATH = "/home/emcj/data/MAG/AuthorsGenderized_merged.csv"
 FILTERED_PATH = "/home/emcj/data/MAG/AuthorsGenderized_filtered.csv"
 OUTPUT_PATH = "/home/emcj/data/MAG/AuthorsGenderized_clean.csv"
 
-# Step 1: Filter rows (exclude unknown genders) and write to temp file
-print("Filtering...")
+print("Filtering")
 df = pl.read_csv(INPUT_PATH, ignore_errors=True, low_memory=True).lazy()
 df_filtered = df.filter(pl.col("Gender").str.to_lowercase() != "unknown")
 df_filtered.collect(streaming=True).write_csv(FILTERED_PATH)
 
-# Step 2: Sort and remove duplicates by DisplayName
-print("Sorting and deduplicating...")
+print("Sorting and deduplicating")
 df_final = (
     pl.read_csv(FILTERED_PATH)
     .sort("DisplayName")  # sort required before deduplication
@@ -19,5 +17,4 @@ df_final = (
 )
 df_final.write_csv(OUTPUT_PATH)
 
-print("Finished writing cleaned CSV:", OUTPUT_PATH)
 
